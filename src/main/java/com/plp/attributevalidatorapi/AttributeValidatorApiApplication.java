017ebfaf887a88fef359b3fcca6b99875a473877
Ruan Carlos <ruan.silva@virtus.ufcg.edu.br>
2023-05-05 00:15:21 -0300

    Added to spring boot.

---
+++ src/main/java/com/plp/attributevalidatorapi/AttributeValidatorApiApplication.java
@@ -0,0 +1,13 @@
+package com.plp.attributevalidatorapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttributeValidatorApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AttributeValidatorApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/plp/attributevalidatorapi/AttributeValidatorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plp.attributevalidatorapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttributeValidatorApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
